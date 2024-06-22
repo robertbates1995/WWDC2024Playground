65ad8c49f2c99f041548dac64d7d8d332fe56494
Robert Bates <thegreatbobini@gmail.com>
2024-06-22 10:58:27 -0700

    created GymSwipe

---
+++ GymSwipe/GymSwipe/GymSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymSwipeApp.swift
+//  GymSwipe
+//
+//  Created by Robert Bates on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct GymSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
