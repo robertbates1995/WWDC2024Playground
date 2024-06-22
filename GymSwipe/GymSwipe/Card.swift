bd4b4d5621ee07e83e7b139612f277331985aae5
Robert Bates <thegreatbobini@gmail.com>
2024-06-22 11:01:17 -0700

    added card to GymSwipe

---
+++ GymSwipe/GymSwipe/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  GymSwipe
+//
+//  Created by Robert Bates on 6/22/24.
+//
+
+import Foundation
+
+struct Card: Codable {
+    var label: String
+    //var image: Image
+    
+    static let example = Card(label: "example label")
+}
